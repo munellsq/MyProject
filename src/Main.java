df03eca57723faff5dce4ff6ebd6bead608f9f04
Mu <nrnbekov@gmail.com>
2024-12-18 16:31:36 +0500

    Update 16:31

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // Создание объектов
+
         Song song1 = new Song("Imagine", "John Lennon", 3.5);
         Song song2 = new Song("Bohemian Rhapsody", "Queen", 5.9);
 
-        // Вывод информации
         song1.displayInfo();
         song2.displayInfo();
 
-        // Сравнение песен
         if (song1.getDuration() > song2.getDuration()) {
             System.out.println(song1.getTitle() + " longer than " + song2.getTitle());
         } else {
